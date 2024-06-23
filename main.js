375610b05a69affa4d3e3b8920b4f13af47bfe9a
Madina_DI <12mileik@rambler.ru>
2024-06-23 20:19:47 +0300

    changed the structure

---
+++ main.js
@@ -0,0 +1,3 @@
+const pointerRight = document.querySelector('right-pointer');
+const pointerLeft = document.querySelector('left-pointer');
+const fotoSlider = document.querySelectorAll('slider-foto');
